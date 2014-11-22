aeabf17559adfcdfd6959d061738c6a076735084
arckauss <boulard.david@gmail.com>
2014-11-22 22:17:03 +0100

    Movements boundaries and fix

--- component_outer_player.lua
+++ component_outer_player.lua
@@ -4,8 +4,10 @@ function ComponentOuterPlayer:init()
     self.total = 0
     self.speed = 0
     self.lastMousePosition = 0
-    self.maxSpeed = 6
+    self.maxSpeed = 10
     self.minSpeed = 1
+    self.leftBoundary = -511
+    self.rightBoundary = 511
 end
 
 function ComponentOuterPlayer:insert()
@@ -13,7 +15,7 @@ end
 
 function ComponentOuterPlayer:update(dt)
     local e = self.entity
-    local speedFactor = -3
+    local speedFactor = -8
     self.total = self.total + dt
 
     local x,y = gengine.input.mouse:getPosition()
@@ -21,17 +23,24 @@ function ComponentOuterPlayer:update(dt)
 
     if wx < 10000 then
         self.speed = (speedFactor * (wx - e.position.x)) * dt
-        -- print(self.speed)
-        -- if self.speed == nil then
-        --     self.speed = 0
-        -- else
-        --     if math.abs(self.speed) > maxSpeed then
-        --         self.speed = self.maxSpeed
-        --     elseif math.abs(self.speed) < minSpeed then
-        --         self.speed = self.minSpeed
-        --     end
-        -- end
+
+        if math.abs(self.speed) < self.minSpeed and math.abs(self.speed) > 0.1 then
+            if self.speed < 0 then
+                self.speed = self.minSpeed * -1
+            else
+                self.speed = self.minSpeed
+            end
+        end
+
         e.position.x = e.position.x - self.speed
+
+        if e.position.x > self.rightBoundary then
+            e.position.x = self.rightBoundary
+        end
+
+        if e.position.x < self.leftBoundary then
+            e.position.x = self.leftBoundary
+        end
     end
 
     if gengine.input.mouse:isJustDown(1) then
